e4f6917c97a91834f60a0e8bb03077ab2100ec81
Kas1o <twins_t@outlook.com>
2024-05-06 10:50:59 +0800

    查看原文功能

--- ReferKing/Program.cs
+++ ReferKing/Program.cs
@@ -1,5 +1,7 @@
 ﻿// 读取书籍
-if(!Path.Exists("./books"))
+using System.Text;
+
+if (!Path.Exists("./books"))
 {
     Console.WriteLine("缺少./books 文件夹，请在其中放置书籍txt");
     Console.Read();
@@ -52,7 +54,17 @@ var random = new Random();
 foreach (var item in refers)
 {
     string quotePhrase = quotePhrases[random.Next(quotePhrases.Length)];
-    Console.WriteLine($"> “{item.content}” {quotePhrase}：{item.bookname}的第{item.idx}字");
+    Console.WriteLine($"### “{item.content}” {quotePhrase}：{item.bookname}的第{item.idx}字");
+    foreach(var book in books)
+    {
+        if(book.bookName == item.bookname)
+        {
+            var near =GetNearbyChars(book.content, item.idx);
+            near = EncapsulateString(near, item.content);
+            near = "> \"" + near + "\"";
+            Console.WriteLine(near);
+        }
+    }
 }
 
 Console.ReadLine();
@@ -91,4 +103,34 @@ Console.ReadLine();
             return (input, idx, (string)bookname);
         }
     }
+}
+static string GetNearbyChars(string x, int n)
+{
+    int start = Math.Max(0, n - 10);
+    int end = Math.Min(x.Length, n + 11);
+    int len = end - start;
+
+    if (len > 20)
+        return x.Substring(start, 20);
+    else
+        return x.Substring(start, len);
+}
+static string EncapsulateString(string b, string a)
+{
+    int index = b.IndexOf(a);
+    StringBuilder sb = new StringBuilder(b);
+
+    while (index != -1)
+    {
+        sb.Insert(index, "**");
+        sb.Insert(index + a.Length + 2, "**");
+
+        int nextIndex = index + a.Length + 4;
+        if (nextIndex >= b.Length)
+            break;
+
+        index = b.IndexOf(a, nextIndex);
+    }
+
+    return sb.ToString();
 }
